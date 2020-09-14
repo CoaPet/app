5086c574d2978da0b2b60306109a276cf94473f8
Philippe NAVEILHAN <travail@naveilhan.com>
2020-09-14 16:13:04 +0200

    rollover

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,7 +25,9 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,
-            'coachInfo' => $coachInfo,]);
+            'coachInfo' => $coachInfo,
+            'title' => 'Connexion',
+            ]);
     }
 
     /**
