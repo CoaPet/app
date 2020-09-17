fcb13de8a519e364cb9a80a3e0b5c2ef2509ad8c
Philippe NAVEILHAN <travail@naveilhan.com>
2020-09-17 10:10:54 +0200

    paginator on actuality

--- src/Controller/ActualityController.php
+++ src/Controller/ActualityController.php
@@ -5,23 +5,34 @@ namespace App\Controller;
 use App\Entity\InfoCoach;
 use App\Repository\ActualityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 class ActualityController extends AbstractController
 {
     /**
      * @Route("/actualite", name="app_actuality")
      */
-    public function index(ActualityRepository $actualityRepository): Response
-    {
+    public function index(
+        ActualityRepository $actualityRepository,
+        request $request,
+        PaginatorInterface $paginator
+    ): Response {
+        $articles = $actualityRepository->findBy([], ['date' => 'DESC']);
+        $pageArticles = $paginator->paginate(
+            $articles,
+            $request->query->getInt('page', 1),
+            5
+        );
         $coachInfo = $this->getDoctrine()
             ->getRepository(InfoCoach::class)
             ->findOneBy([]);
 
         return $this->render('actuality/index.html.twig', [
             'coachInfo' => $coachInfo,
-            'actualities' => $actualityRepository->findBy([], ['date' => 'desc']),
+            'actualities' => $pageArticles,
             'title' => 'Actualités',
             'underTitle' => 'Suivez mes conseils, au jour le jour',
         ]);
