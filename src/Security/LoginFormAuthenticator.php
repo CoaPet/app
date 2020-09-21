d3f5d4bb3efcd172b8013903cfe6c53621a2145e
Philippe NAVEILHAN <travail@naveilhan.com>
2020-09-21 21:05:51 +0200

    fix home

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -101,7 +101,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_index'));
+        return new RedirectResponse($this->urlGenerator->generate('app_member'));
     }
 
     protected function getLoginUrl()
